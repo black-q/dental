857f555a67f9e16a099666abcda551c20831e933
Damian <damian.wieckowski@gmail.com>
2020-08-20 12:57:12 +0200

    production

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+"""
+Initialization file.
+"""
+
+from flask import Flask
+import os
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='dev')
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+    from . import views
+    app.register_blueprint(views.bp)
+
+    return app
